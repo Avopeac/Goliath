0692a340236568245b1553541bc61ffc0df06f78
Andreas Larsson <avopeac@gmail.com>
2015-11-19 16:06:46 +0100

    Enabled atmosphere shader again.

--- Source/View/Renderer.cpp
+++ Source/View/Renderer.cpp
@@ -16,7 +16,7 @@ void Renderer::add_drawable(const std::shared_ptr<Drawable> &drawable) {
 
 void Renderer::initialize() {
 	//Set up post processing
-	//_post_processing.add_node(std::make_shared<AtmosphereNode>());
+	_post_processing.add_node(std::make_shared<AtmosphereNode>());
 	_post_processing.add_node(std::make_shared<BloomNode>(2, 0.42f, 1.0f, 1.0f));
 	_post_processing.add_node(std::make_shared<GammaToneMapNode>(2.3f, 2.2f));
 }

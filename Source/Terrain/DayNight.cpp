567ccf75142a87b71f6d48818b4946e86b7e070d
Max Halldén <max.hallden@gmail.com>
2015-12-09 18:57:58 +0100

    Added DayNight stub

---
+++ Source/Terrain/DayNight.cpp
@@ -0,0 +1,8 @@
+﻿#include "DayNight.h"
+
+DayNight::DayNight() : _sun(0,1,0) {
+}
+
+glm::dvec3 DayNight::get_sun() {
+	return _sun;
+}
---
+++ Source/Terrain/DayNight.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <GLM/vec3.hpp>
+
+class DayNight {
+public:
+	//Singleton
+	static DayNight &instance() {
+		static DayNight instance;
+		return instance;
+	}
+
+	glm::dvec3 get_sun();
+
+protected:
+	DayNight();
+
+private:
+	glm::dvec3 _sun;
+};
